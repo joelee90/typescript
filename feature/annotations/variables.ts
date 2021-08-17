2850b5a52a65d97afc1fbcceba26aa40773c8a90
joelee90 <dlwhdgur@h-brick.com>
2021-08-17 22:58:51 +0900

    Annotations with variables

---
+++ feature/annotations/variables.ts
@@ -0,0 +1,7 @@
+// Annotations with variables
+let apple: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let noth: null = null;
+let nothing: undefined = undefined;
+let now: Date = new Date();
