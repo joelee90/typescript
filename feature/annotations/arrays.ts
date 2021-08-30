4662944b184750ab53205508b36bd1ed6e5e8d8e
joelee90 <jonghyeoklee90@gmail.com>
2021-08-30 23:48:16 +0900

    arrays in TS

---
+++ feature/annotations/arrays.ts
@@ -0,0 +1,33 @@
+// Arrays : Each element is some consisten type of value
+const carMakers = ['ford', 'toyota', 'chevy'];
+const carByMake: string[][] = [];
+const carByMakee = [
+    ['explorer'],
+    ['corolla'],
+    ['bumblebee'],
+];
+// Declare empty array -> assign type else not needed.
+
+// Why Typed Arrays?
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100)
+
+// Help with 'map'
+const b = carMakers.map((car: string): string => {
+    return car.toUpperCase();
+})
+console.log(b)
+
+// Flexible types
+const importantDates: (Date | String)[] = [new Date()];
+importantDates.push(new Date());
+importantDates.push('3');
+importantDates.push(1002);
+
+// Tuples in TypeScript(Typed Arrays)
+// Any time we need to represent a collection of records with some arbitrary sort order
+// Array-like structure where each element represents some property of a record
--- feature/annotations/functions.ts
+++ feature/annotations/functions.ts
@@ -1,12 +1,12 @@
 // argument list: annotation every single argumen + annotation for what the function will return.
 // TS will analyze the body of the function. TS doesn't check the logic.
 const add = (a: number, b: number): number => {
- return a + b; 
+    return a + b;
 }
 
 // void
 const subtract = (a: number, b: number) => {
-    a - b; 
+    a - b;
 }
 
 const logger = (message: string): void => {
@@ -19,12 +19,12 @@ const forecast = {
     weather: 'sunny'
 }
 
-const logWeather = (forecast : {date: Date, weather: string}): void => {
+const logWeather = (forecast: { date: Date, weather: string }): void => {
     console.log(`weather: ${forecast.weather}`)
 }
 
 // destructuring
-const logWeatherDes = ({date, weather} : {date: Date, weather: string}): void => {
+const logWeatherDes = ({ date, weather }: { date: Date, weather: string }): void => {
     console.log(date)
     console.log(weather)
 }
@@ -39,10 +39,12 @@ const profile = {
         lat: 0,
         lng: 15
     },
-    setAge (age: number): void {
+    setAge(age: number): void {
         this.age = age;
     }
 }
 
-const {age, name} : {age: number, name: string, } = profile;
-const {coords: {lat, lng}} : {coords:{ lat: number, lng: number}} = profile;
+const { age, name }: { age: number, name: string, } = profile;
+const { coords: { lat, lng } }: { coords: { lat: number, lng: number } } = profile;
+
+
