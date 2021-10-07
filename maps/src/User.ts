52c06f7508892a4253bde3bde00201759bc89f6d
joelee90 <dlwhdgur@h-brick.com>
2021-10-07 23:29:50 +0900

    Export statements in TS

---
+++ maps/src/User.ts
@@ -0,0 +1,31 @@
+// In general, any file that houses class in single file in TS, import the class from elsewhere and use it there.
+
+import faker from 'faker';
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
+
+// # 205
+// Constructor : special function defined inside of a class.
+// Anytime constructor is defined, it will be instantly executed.
+// right when we create instance of a class.
+// When new class is created, constructor fucntion is going to run./
+// this.property = some value -> initial assginment.
+
+// # 206
+// whenever call the constructor of a child class, required to call the constructor
+// of a parent class as well.
+// => super() : reference to the constructor method of a parent.
